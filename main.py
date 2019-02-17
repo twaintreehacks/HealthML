b3cbc2c649288622451f10301ec637e5c8d67391
Grant Hugh <granthugh@Grants-MacBook-Pro.local>
2019-02-16 16:37:43 -0800

    More data processing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import keras as k
+import numpy as np
 from random import shuffle
 
 file = open("heart.csv", "r")
@@ -23,7 +24,35 @@ shuffle(data)
 trainingData = data[:200]
 testData = data[200:]
 
+trainingVals = []
+trainingResults = []
 
+for d in trainingData:
+    vals = list(d.values())
+    trainingVals.append(vals[:-1])
+    trainingResults.append(vals[-1])
+
+testVals = []
+testResults = []
+
+for d in testData:
+    vals = list(d.values())
+    testVals.append(vals[:-1])
+    testResults.append(vals[-1])
+
+model = k.models.Sequential([
+    k.layers.Dense(64, input_dim = len(trainingVals[0])),
+    k.layers.Dense(1)
+])
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(np.array(trainingVals), np.array(trainingResults), epochs=10, batch_size=32)
+
+print(model.predict(np.array(testVals)))
+print(testResults)
 
 
 
