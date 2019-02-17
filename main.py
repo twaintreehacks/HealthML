2b6d1c182c7d4cca9669a44d8d50129950eb948d
Grant Hugh <granthugh@Grants-MacBook-Pro.local>
2019-02-16 17:03:43 -0800

    Neural network sort of working, best averages around 66% accuracy

--- main.py
+++ main.py
@@ -30,7 +30,11 @@ trainingResults = []
 for d in trainingData:
     vals = list(d.values())
     trainingVals.append(vals[:-1])
-    trainingResults.append(vals[-1])
+    if vals[-1] == '1':
+        trainingResults.append([1, 0])
+    else:
+        trainingResults.append([0, 1])
+
 
 testVals = []
 testResults = []
@@ -38,18 +42,23 @@ testResults = []
 for d in testData:
     vals = list(d.values())
     testVals.append(vals[:-1])
-    testResults.append(vals[-1])
+    if vals[-1] == '1':
+        testResults.append([1, 0])
+    else:
+        testResults.append([0, 1])
 
 model = k.models.Sequential([
     k.layers.Dense(64, input_dim = len(trainingVals[0])),
-    k.layers.Dense(1)
+    k.layers.Activation('softmax'),
+    k.layers.Dense(2),
+    k.layers.Activation('softmax')
 ])
 
 model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.fit(np.array(trainingVals), np.array(trainingResults), epochs=10, batch_size=32)
-
+model.fit(np.array(trainingVals), np.array(trainingResults), epochs=20, batch_size=8)
 print(model.predict(np.array(testVals)))
 print(testResults)
+print(model.evaluate(np.array(testVals), np.array(testResults)))
